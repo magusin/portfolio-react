37283689cec7bf21d342d32e89adc3c8ea6445aa
magusin <fanatsy4@gmail.com>
2021-10-30 21:44:27 +0200

    add mouseparticle

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import Nav from "./components/Nav";
 import React, { Component }  from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import MouseParticles from 'react-mouse-particles'
 
 function App() {
   return (
     <>
+    <MouseParticles g={1} color="random" cull="col,image-wrapper"/>
     <Router>
         <header>
             <Nav/>
