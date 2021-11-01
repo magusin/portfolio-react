086cabb9c9d324a1912393501d666b4937cab6af
magusin <fanatsy4@gmail.com>
2021-11-01 18:00:58 +0100

    fix html, css logo

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -3,80 +3,79 @@ import React, { Component } from "react";
 import "./skills.css";
 
 const skills = () => {
-//   document.addEventListener("click", function() {
-//     start();
-//   });
-//   function start() {
-//     gsap.to(".container-skills", {
-//        rotation: "+=90"   
-//     })
-// }
+  //   document.addEventListener("click", function() {
+  //     start();
+  //   });
+  //   function start() {
+  //     gsap.to(".container-skills", {
+  //        rotation: "+=90"
+  //     })
+  // }
 
   return (
     <>
-   <div className="skills">
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front html"></div>
-           <div class="back html-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front css"></div>
-           <div class="back css-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front php"></div>
-           <div class="back php-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front js"></div>
-           <div class="back js-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front react"></div>
-           <div class="back react-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front sql"></div>
-           <div class="back sql-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front sass"></div>
-           <div class="back sass-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front tailwind"></div>
-           <div class="back tailwind-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front gsap"></div>
-           <div class="back gsap-back"></div>
-       </div>
-   </div>
-   <div class="container-skills">
-       <div class="card-skills">
-           <div class="front bootstrap"></div>
-           <div class="back bootstrap-back"></div>
-       </div>
-   </div>
-   </div>
-   
+      <div className="skills">
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front html font-sans font-bold"></div>
+            <div class="back html-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front css"></div>
+            <div class="back css-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front php"></div>
+            <div class="back php-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front js"></div>
+            <div class="back js-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front react"></div>
+            <div class="back react-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front sql"></div>
+            <div class="back sql-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front sass"></div>
+            <div class="back sass-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front tailwind"></div>
+            <div class="back tailwind-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front gsap"></div>
+            <div class="back gsap-back"></div>
+          </div>
+        </div>
+        <div class="container-skills">
+          <div class="card-skills">
+            <div class="front bootstrap"></div>
+            <div class="back bootstrap-back"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
