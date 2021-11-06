3ab30dd7aaec4f3916ac89cf626989a17573fbfe
magusin <fanatsy4@gmail.com>
2021-11-06 02:30:44 +0100

    complete data.json

--- src/components/aboutme/renderToSprite.js
+++ src/components/aboutme/renderToSprite.js
@@ -9,6 +9,6 @@ export default async function renderToSprite(content, { width, height }) {
   const map = new THREE.CanvasTexture(canvas);
   const material = new THREE.SpriteMaterial({ map });
   const sprite = new THREE.Sprite(material);
-  sprite.scale.set(width / 6, height / 6);
+  sprite.scale.set(width / 5, height / 5);
   return sprite;
 }
